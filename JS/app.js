44ea028e370f76ca93d66a5378cca3cd0095bef1
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-03 17:03:05 +0600

    Prind Search Result function added

--- JS/app.js
+++ JS/app.js
@@ -19,3 +19,29 @@ const searchPhone = () => {
   }
   searchInput.value = "";
 };
+
+/* -------------- Print Search Result in the Result Div -------------- */
+const printPhone = (phones) => {
+  phoneContainer.textContent = "";
+  detailsContainer.textContent = "";
+  const limitedItem = phones.slice(0, 20)
+  if (phones.length == 0) {
+    errorMsg.innerHTML = ` <h2>No, result Found! 😥</h2>`;
+  } else {
+    limitedItem.forEach((phon) => {
+      const div = document.createElement("div");
+      div.className = "col";
+      div.innerHTML = `
+        <div class="card shadow border-2">
+          <img src="${phon.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h3 class="card-title">${phon.phone_name}</h3>
+            <p><span class="fw-bold">Brand:</span> ${phon.brand}</p>
+            <button class="fw-bold btn btn-info" onclick="printDetals('${phon.slug}')">Explore</button>
+          </div>
+        </div>
+        `;
+      phoneContainer.appendChild(div);
+    });
+  }
+};
